7b2d4c32cfb1748523e69e40cac93d82fbe61c38
Felix Almejo <fd.almejo@gmail.com>
2021-08-05 16:04:23 -0700

    concatenated from three templates to a single template

--- server.py
+++ server.py
@@ -1,16 +1,19 @@
 from flask import Flask, render_template
 app = Flask(__name__)
 
+# @app.route('/play')
+# def on_load():
+#     return render_template("index.html", num = 3)
+
+# @app.route('/play/<int:num>')
+# def add_squares(num):
+#     return render_template("index.html", num = num)
+
+# Sensei Bonus
 @app.route('/play')
-def on_load():
-    return render_template("index.html", num = 3)
-
 @app.route('/play/<int:num>')
-def add_squares(num):
-    return render_template("index.html", num = num)
-
 @app.route('/play/<int:num>/<string:color>')
-def change_color(num,color):
+def change_color(num = 3,color = 'lightblue'):
     return render_template("index.html", num = num, color=color)
 
 if __name__=="__main__":
